b751818ba74cfed24fbf7a798d0492b9b10a6cda
Kiki <kiki2002kimo@gmail.com>
2017-11-22 09:04:49 +0800

    Add test :: WhenKeyMessageAreEmpty_ReturnEmpty (failed)

--- BasicNicoVariation.cs
+++ BasicNicoVariation.cs
@@ -2,7 +2,16 @@
 
 namespace BasicNicoVariation
 {
-    public class BasicNicoVariation
+    public class Nico
     {
+        public Nico(string key, string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Variation()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- BasicNicoVariationTest.cs
+++ BasicNicoVariationTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -10,5 +11,16 @@ namespace BasicNicoVariation
     [TestFixture]
     public class BasicNicoVariationTest
     {
+        [Test]
+        public void WhenKeyMessageAreEmpty_ReturnEmpty()
+        {
+            var key = string.Empty;
+            var message = string.Empty;
+            var nico = new Nico(key, message);
+
+            var expected = string.Empty;
+
+            Assert.AreEqual(expected, nico.Variation());
+        }
     }
 }
